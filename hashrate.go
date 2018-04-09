9f6ad1849847f6eb53621fc217199f1d4dec5491
Gerald Bauer <gerald.bauer@gmail.com>
2018-04-09 22:04:49 +0200

    Update hashrate.go

--- hashrate.go
+++ hashrate.go
@@ -69,3 +69,40 @@ func main() {
      }
   }
 }
+
+
+// Difficulty: 0 (4 bits)
+// Difficulty: 00 (8 bits)
+// Difficulty: 000 (12 bits)
+// Difficulty: 0000 (16 bits)
+// Difficulty: 00000 (20 bits)
+// Difficulty: 000000 (24 bits)
+//
+// Difficulty: 0 (4 bits)
+// Starting search...
+// Elapsed Time: 0s, Hashes Calculated: 56
+//
+// Difficulty: 00 (8 bits)
+// Starting search...
+// Elapsed Time: 1.0001ms, Hashes Calculated: 143
+// Hash Rate: 142985 hashes per second
+//
+// Difficulty: 000 (12 bits)
+// Starting search...
+// Elapsed Time: 5.0002ms, Hashes Calculated: 3834
+// Hash Rate: 766769 hashes per second
+//
+// Difficulty: 0000 (16 bits)
+// Starting search...
+// Elapsed Time: 33.0019ms, Hashes Calculated: 26762
+// Hash Rate: 810923 hashes per second
+//
+// Difficulty: 00000 (20 bits)
+// Starting search...
+// Elapsed Time: 137.0078ms, Hashes Calculated: 118592
+// Hash Rate: 865585 hashes per second
+//
+// Difficulty: 000000 (24 bits)
+// Starting search...
+// Elapsed Time: 26.5378148s, Hashes Calculated: 21554046
+// Hash Rate: 812201 hashes per second
